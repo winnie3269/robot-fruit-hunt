4e7fb882028915a248917770cfc4174e7262c797
winnie3269 <wang.winnie26@gmail.com>
2016-04-06 11:39:20 -0700

    Update player_1.js

--- player_1.js
+++ player_1.js
@@ -9,43 +9,43 @@ function make_move() {
        return TAKE;
    }
    var rand = Math.random() * 4;
-   for(var i =0; i<board[x].length; i++)
+   /*for(var i =0; i<board[x].length; i++)
    {
       for(var n=0; n<board[y].length; n++)
-         {
+         {*/
    if(has_item(button[get_my_x()][get_my_y()]))
       return TAKE;
-   else if(has_item(button[get_my_x()+n][get_my_y()]))
+   else if(has_item(button[get_my_x()+1][get_my_y()]))
       return EAST;
-   else if(has_item(button[get_my_x()+n][get_my_y()+i]))
+   else if(has_item(button[get_my_x()+1][get_my_y()+1]))
    {
       return EAST;
       return PASS;
       return SOUTH;
    }
-   else if(has_item(button[get_my_x()+n][get_my_y()-i]))
+   else if(has_item(button[get_my_x()+1][get_my_y()-1]))
    {
       return EAST;
       return PASS;
       return NORTH;
    }
-   else if(has_item(button[get_my_x()-n][get_my_y()]))
+   else if(has_item(button[get_my_x()-1][get_my_y()]))
       return WEST;
-   else if(has_item(button[get_my_x()-n][get_my_y()+i]))
+   else if(has_item(button[get_my_x()-1][get_my_y()+1]))
    {
       return WEST;
       return PASS;
       return SOUTH;
    }
-   else if(has_item(button[get_my_x()-n][get_my_y()-i]))
+   else if(has_item(button[get_my_x()-1][get_my_y()-1]))
    {
       return WEST;
       return PASS;
       return NORTH;
    }
-   else if(has_item(button[get_my_x()][get_my_y()+i]))
+   else if(has_item(button[get_my_x()][get_my_y()+1]))
       return SOUTH;
-   else if(has_item(button[get_my_x()][get_my_y()-i]))
+   else if(has_item(button[get_my_x()][get_my_y()-1]))
       return NORTH;
    else 
    {
@@ -54,8 +54,8 @@ function make_move() {
       if (rand < 3) return EAST;
       if (rand < 4) return WEST;
    }
-         }
-   }
+  /*       }
+   }*/
    
   
   /* if (rand < 1) return NORTH;
